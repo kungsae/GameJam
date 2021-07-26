a0b429d94dd0faa6b87b02f8aa526981b4994ed9
DESKTOP-77DPHAE\ROG <3soojung@gmail.com>
2021-07-27 01:32:12 +0900

    엑조디아 재구성

--- Assets/01.Script/Ingame/Fusion.cs
+++ Assets/01.Script/Ingame/Fusion.cs
@@ -12,56 +12,81 @@ public class Fusion : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        joint = GetComponent<HingeJoint2D>();
-        joint.autoConfigureConnectedAnchor = true;
-        joint.connectedBody = body.GetComponent<Rigidbody2D>();
-        bodyHeight = body.GetComponent<SpriteRenderer>().bounds.size.y;
-		switch (part)
-		{
-            case "Head":
-                StartCoroutine(Headfollow());
-                break;
-            case "Leg":
-                LegFollow();
-                break;
-            case "Calf":
-                StartCoroutine(CalfFollow());
-                break;
-            case "Arm":
-                StartCoroutine(ArmFollow());
-                break;
-            default:
-                Debug.LogError("어느 파츠에도 포함되지 않습니다");
-				break;
-		}
-        StartCoroutine(JointFollow());
-        
+        if (part == "Body")
+        {
+            gameObject.name = "Body";
+        }
+        else
+        {
+            body = GameObject.Find("Body");
+            Debug.Log(body);
+            joint = GetComponent<HingeJoint2D>();
+            joint.autoConfigureConnectedAnchor = true;
+
+            bodyHeight = body.GetComponent<SpriteRenderer>().bounds.size.y;
+            Debug.Log(bodyHeight);
+            switch (part)
+            {
+                case "Head":
+                    gameObject.name = "Head";
+                    StartCoroutine(Headfollow());
+                    break;
+                case "LegR":
+                    gameObject.name = "LegR";
+                    StartCoroutine(LegFollow());
+                    break;
+                case "LegL":
+                    gameObject.name = "LegL";
+                    StartCoroutine(LegFollow());
+                    break;
+                case "CalfR":
+                    gameObject.name = "CalfR";
+                    StartCoroutine(CalfFollow("LegR"));
+                    break;
+                case "CalfL":
+                    gameObject.name = "CalfL";
+                    StartCoroutine(CalfFollow("LegL"));
+                    break;
+                case "Arm":
+                    StartCoroutine(ArmFollow());
+                    gameObject.name = "Arm";
+                    break;
+                default:
+                    Debug.LogError("어느 파츠에도 포함되지 않습니다");
+                    break;
+            }
+            
+            StartCoroutine(JointFollow());
+        }
     }
-    public void LegFollow()
+	IEnumerator LegFollow()
     {
+        yield return new WaitForSeconds(0.01f);
         float height = GetComponent<SpriteRenderer>().bounds.size.y;
         joint.anchor = new Vector2(0, (height));
         transform.position = body.transform.position + new Vector3(0, -(bodyHeight * 0.5f), 0) + new Vector3(0, -(height * 0.5f), 0);
     }
     IEnumerator Headfollow()
     {
-        yield return new WaitForSeconds(0.01f);
+        yield return new WaitForSeconds(0.02f);
         
         float height = GetComponent<SpriteRenderer>().bounds.size.y;
         joint.anchor = new Vector2(0,-(height * 0.5f));
         transform.position = body.transform.position + new Vector3(0, bodyHeight * 0.5f, 0) + new Vector3(0, height*0.5f,0);
     }
-    IEnumerator CalfFollow()
+    IEnumerator CalfFollow(string legName)
     {
-        yield return new WaitForSeconds(0.01f);
-
+        yield return new WaitForSeconds(0.03f);
+        FindLeg(legName);
         float height = GetComponent<SpriteRenderer>().bounds.size.y;
+        bodyHeight = body.GetComponent<SpriteRenderer>().bounds.size.y;
         joint.anchor = new Vector2(0, height);
         transform.position = body.transform.position + new Vector3(0, -(bodyHeight * 0.5f), 0) + new Vector3(0, -(height * 0.5f), 0);
+
     }
     IEnumerator ArmFollow()
     {
-        yield return new WaitForSeconds(0.01f);
+        yield return new WaitForSeconds(0.02f);
 
         float height = GetComponent<SpriteRenderer>().bounds.size.x;
         joint.anchor = new Vector2(-(height*0.5f),0);
@@ -70,7 +95,16 @@ public class Fusion : MonoBehaviour
     IEnumerator JointFollow()
     {
         yield return new WaitForSeconds(0.05f);
+        joint.connectedBody = body.GetComponent<Rigidbody2D>();
         joint.autoConfigureConnectedAnchor = false;
+        if (part == "CalfL" || part == "CalfR")
+        {
+            joint.connectedAnchor = new Vector2(0, -0.5f);
+        }
+    }
+    private void FindLeg(string legName)
+    {
+        body = GameObject.Find(legName);
     }
 
 }
--- Assets/01.Script/Ingame/Player.cs
+++ Assets/01.Script/Ingame/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     Rigidbody2D rig;
     public Rigidbody2D[] leg;
+    private HingeJoint2D[] legJoint = new HingeJoint2D[4];
     public Rigidbody2D[] arm;
     public float time = 0.1f;
     public float power;
@@ -14,11 +15,17 @@ public class Player : MonoBehaviour
     bool isGround = true;
     bool isDead = false;
     Vector3 legPos;
+    JointAngleLimits2D limit;
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         legPos = new Vector3(0,leg[3].GetComponent<SpriteRenderer>().bounds.size.y);
         StartCoroutine(stand());
+        limit.min = 0;
+        for (int i = 0; i < leg.Length; i++)
+		{
+            legJoint[i] = leg[i].GetComponent<HingeJoint2D>();
+        }
     }
 
     // Update is called once per frame
@@ -27,16 +34,44 @@ public class Player : MonoBehaviour
         if (!isDead)
         {
             isGround = Physics2D.Raycast(leg[3].transform.position - legPos, Vector2.down, 0.1f, 7);
+
             if (Input.GetKey(KeyCode.A))
             {
+                limit.max = -60;
+                legJoint[2].limits = limit;
+                legJoint[3].limits = limit;
                 leg[0].AddTorque(power);
                 leg[1].AddTorque(-power);
             }
-            if (Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.S))
             {
+                limit.max = -60;
+                legJoint[2].limits = limit;
+                legJoint[3].limits = limit;
                 leg[0].AddTorque(-power);
                 leg[1].AddTorque(power);
             }
+            if (Input.GetKey(KeyCode.D))
+            {
+                limit.max = 60;
+                legJoint[2].limits = limit;
+                legJoint[3].limits = limit;
+
+
+                leg[0].AddTorque(power);
+                leg[1].AddTorque(-power);
+            }
+            if (Input.GetKey(KeyCode.F))
+            {
+                limit.max = 60;
+                legJoint[2].limits = limit;
+                legJoint[3].limits = limit;
+
+                leg[0].AddTorque(-power);
+                leg[1].AddTorque(power);
+            }
+           
+
             if (Input.GetKey(KeyCode.LeftArrow))
             {
                 arm[0].AddTorque(armPower);
@@ -47,6 +82,7 @@ public class Player : MonoBehaviour
                 arm[0].AddTorque(-armPower);
                 arm[1].AddTorque(-armPower);
             }
+
 
             if (Input.GetKey(KeyCode.Space))
             {
