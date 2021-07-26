646185d56b0d7acb7f4786a02b951325401452eb
sunwoong-m <77255000+sunwoong-m@users.noreply.github.com>
2021-07-26 17:12:39 +0900

    부위 선택창

---
+++ Assets/01.Script/SelectManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectManager : MonoBehaviour
+{
+    public void TestLog()
+    {
+        Debug.Log("pressed");
+    }
+}
