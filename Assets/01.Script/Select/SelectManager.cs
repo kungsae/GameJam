f7a953854921261cc9defd1119e6e776a2d06a95
sunwoong-m <77255000+sunwoong-m@users.noreply.github.com>
2021-07-28 01:34:45 +0900

    커스터마이징 씬 수정

--- Assets/01.Script/Select/SelectManager.cs
+++ Assets/01.Script/Select/SelectManager.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 
 public class SelectManager : MonoBehaviour
 {
-    int a = 0;
-
     [Header("캐릭터 부위 관련 (0 : Left, 1 : Right)")]
     public GameObject body;
     public GameObject head;
@@ -15,90 +13,77 @@ public class SelectManager : MonoBehaviour
     public GameObject[] calfs = new GameObject[2];
 
     [Header("캐릭터 부위 선택 관련")]
-    public RectTransform[] selectParts = new RectTransform[8];
     public SpriteRenderer[] charParts = new SpriteRenderer[8];
-    public List<Button> selectBtns = new List<Button>();
 
     [Header("캐릭터 부위 프리팹")]
+    public List<Image> partImages = new List<Image>();
     public List<Image> partPrefabs = new List<Image>();
+
+    int partsValue = 0;
 
     void Start()
     {
-        for(int i = 0; i < 8; i++)
+        for (int i = 0; i < 8; i++)
         {
+            partImages[i].sprite = partPrefabs[0].sprite;
             charParts[i].sprite = partPrefabs[0].sprite;
             GameManager.Instance.character[i] = (0);
             Custom(i);
         }
     }
 
-    public float IntRound(float Value, int Digit)
-    {
-        float Temp = Mathf.Pow(10.0f, Digit);
-        return Mathf.Round(Value * Temp) / Temp;
-    }
-
-
     public void Custom(int n)
     {
         float bodyHeight = body.GetComponent<SpriteRenderer>().bounds.size.y;//o
+        float headHeight = head.GetComponent<SpriteRenderer>().bounds.size.y;
+        float armLHeight = arms[0].GetComponent<SpriteRenderer>().bounds.size.y;
+        float armRHeight = arms[1].GetComponent<SpriteRenderer>().bounds.size.y;
+        float thighLHeight = thighs[0].GetComponent<SpriteRenderer>().bounds.size.y;
+        float thighRHeight = thighs[1].GetComponent<SpriteRenderer>().bounds.size.y;
+        float calfLHeight = calfs[0].GetComponent<SpriteRenderer>().bounds.size.y;
+        float calfRHeight = calfs[1].GetComponent<SpriteRenderer>().bounds.size.y;
 
-        if (n == 1)
-        {
-            float headHeight = head.GetComponent<SpriteRenderer>().bounds.size.y;
-            head.transform.position = body.transform.position + new Vector3(0, bodyHeight * 0.5f, 0) + new Vector3(0, headHeight * 0.5f, 0);//o
-        }
-        else if (n == 2)
-        {
-            float armLHeight = arms[0].GetComponent<SpriteRenderer>().bounds.size.y;
-            arms[0].transform.position = body.transform.position + new Vector3(-(bodyHeight * 0.5f), 0, 0) + new Vector3(-(armLHeight * 0.5f), 0, 0);
-        }
-        else if (n == 3)
-        {
-            float armRHeight = arms[1].GetComponent<SpriteRenderer>().bounds.size.y;
-            arms[1].transform.position = body.transform.position + new Vector3(bodyHeight * 0.5f, 0, 0) + new Vector3(armRHeight * 0.5f, 0, 0);
-        }
-        else if (n == 4)
-        {
-            float thighLHeight = thighs[0].GetComponent<SpriteRenderer>().bounds.size.y;
-            thighs[0].transform.position = body.transform.position + new Vector3(0, -(bodyHeight * 0.5f), 0) + new Vector3(-(thighLHeight * 0.5f), -(thighLHeight * 0.5f), 0);
-        }
-        else if (n == 5)
-        {
-            float thighRHeight = thighs[1].GetComponent<SpriteRenderer>().bounds.size.y;
-            thighs[1].transform.position = body.transform.position + new Vector3(0, -(bodyHeight * 0.5f), 0) + new Vector3(thighRHeight * 0.5f, -(thighRHeight * 0.5f), 0);
-        }
-        else if (n == 6)
-        {
-            float calfLHeight = calfs[0].GetComponent<SpriteRenderer>().bounds.size.y;
-            calfs[0].transform.position = body.transform.position + new Vector3(0, -(bodyHeight * 0.5f), 0) + new Vector3(-(calfLHeight * 0.5f), -(calfLHeight * 1.5f), 0);
-        }
-        else if (n == 7)
-        {
-            float calfRHeight = calfs[1].GetComponent<SpriteRenderer>().bounds.size.y;
-            calfs[1].transform.position = body.transform.position + new Vector3(0, -(bodyHeight * 0.5f), 0) + new Vector3(calfRHeight * 0.5f, -(calfRHeight * 1.5f), 0);
-        }
+        head.transform.position = body.transform.position + new Vector3(0, bodyHeight * 0.5f, 0) + new Vector3(0, headHeight * 0.5f, 0);//o
+        arms[0].transform.position = body.transform.position + new Vector3(-(bodyHeight * 0.5f), 0, 0) + new Vector3(-(armLHeight * 0.5f), 0, 0);
+        arms[1].transform.position = body.transform.position + new Vector3(bodyHeight * 0.5f, 0, 0) + new Vector3(armRHeight * 0.5f, 0, 0);
+        thighs[0].transform.position = body.transform.position + new Vector3(0, -(bodyHeight * 0.5f), 0) + new Vector3(-(thighLHeight * 0.5f), -(thighLHeight * 0.5f), 0);
+        thighs[1].transform.position = body.transform.position + new Vector3(0, -(bodyHeight * 0.5f), 0) + new Vector3(thighRHeight * 0.5f, -(thighRHeight * 0.5f), 0);
+        calfs[0].transform.position = body.transform.position + new Vector3(0, -(bodyHeight * 0.5f), 0) + new Vector3(-(calfLHeight * 0.5f), -(calfLHeight * 1.5f), 0);
+        calfs[1].transform.position = body.transform.position + new Vector3(0, -(bodyHeight * 0.5f), 0) + new Vector3(calfRHeight * 0.5f, -(calfRHeight * 1.5f), 0);
     }
 
-    public void SelectLPart(int n)
+    public void RMoveParts(int n)
     {
-        Debug.Log(((int)selectParts[n].position.x + 301));  
-        a = (int)IntRound((int)selectParts[n].position.x + 301, -1) / 240;
-        Debug.Log(a);
+        if (partsValue > partPrefabs.Count - 1)
+        {
+            partsValue = 0;
+        }
+        else
+        {
+            partsValue++;
+        }
 
-        Debug.Log((-a + 3) + "a");
-        charParts[n].sprite = partPrefabs[-a + 3].sprite;
-        GameManager.Instance.character[n] = (-a + 3);
+        GameManager.Instance.character[n] = partsValue;
+        partImages[n].
+        partImages[n].sprite = partPrefabs[partsValue].sprite;
+        charParts[n].sprite = partPrefabs[partsValue].sprite;
+        Custom(partsValue);
     }
 
-    public void SelectRPart(int n)
+    public void LMoveParts(int n)
     {
-        Debug.Log(IntRound(((int)selectParts[n].position.x), -1));
-        a = (int)IntRound(((int)selectParts[n].position.x - 360), -1) / 240;
-        Debug.Log(a);
-
-        Debug.Log((-a + 3) + "a");
-        charParts[n].sprite = partPrefabs[-a + 3].sprite;
-        GameManager.Instance.character[n] = (-a + 3);
+        if (partsValue < 0)
+        {
+            partsValue = partPrefabs.Count - 1;
+        }
+        else
+        {
+            partsValue--;
+        }
+        
+        GameManager.Instance.character[n] = partsValue;
+        partImages[n].sprite = partPrefabs[partsValue].sprite;
+        charParts[n].sprite = partPrefabs[partsValue].sprite;
+        Custom(partsValue);
     }
 }
