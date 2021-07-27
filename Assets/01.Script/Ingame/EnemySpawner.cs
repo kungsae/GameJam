7f6e3d9041fa28090e0ed405a096102384c782c6
DESKTOP-77DPHAE\ROG <3soojung@gmail.com>
2021-07-27 23:59:49 +0900

    새씬

--- Assets/01.Script/Ingame/EnemySpawner.cs
+++ Assets/01.Script/Ingame/EnemySpawner.cs
@@ -13,6 +13,7 @@ public class EnemySpawner : MonoBehaviour
     public List<GameObject> partsList = new List<GameObject>();
     public int enemyNum;
     public int EnemyCount;
+    public bool isBattle = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,29 +23,36 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     IEnumerator SpawnEnemy(int enemyCount)
     {
-        yield return new WaitForSeconds(5f);
-        for (int j = 0; j < enemyCount; j++)
+        while (true)
         {
-            transform.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0);
-            yield return new WaitForSeconds(0.01f);
-            enemyNum++;           
-            for (int i = 0; i < parts.Length; i++)
+            yield return new WaitForSeconds(5f);
+            for (int j = 0; j < enemyCount; j++)
             {
-                Rigidbody2D rig = parts[i].GetComponent<Rigidbody2D>();
-                rig.gravityScale = 0;
-                rig.velocity = Vector2.zero;
-                rig.constraints = RigidbodyConstraints2D.FreezeRotation;
+                transform.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0);
+                yield return new WaitForSeconds(0.01f);
+                enemyNum++;
+                for (int i = 0; i < parts.Length; i++)
+                {
+                    Rigidbody2D rig = parts[i].GetComponent<Rigidbody2D>();
+                    rig.gravityScale = 0;
+                    rig.velocity = Vector2.zero;
+                    rig.constraints = RigidbodyConstraints2D.FreezeRotation;
+                }
+                for (int i = 0; i < parts.Length; i++)
+                {
+                    yield return new WaitForSeconds(0.5f);
+                    GameObject part = Instantiate(parts[i], transform.position, transform.rotation);
+                    partsList.Add(part);
+                }
+                for (int i = 0; i < partsList.Count; i++)
+                {
+                    partsList[i].GetComponent<Rigidbody2D>().gravityScale = 1;
+                    partsList[i].GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+                }
             }
-            for (int i = 0; i < parts.Length; i++)
+            if (!isBattle)
             {
-                yield return new WaitForSeconds(0.5f);
-                GameObject part = Instantiate(parts[i],transform.position,transform.rotation);
-                partsList.Add(part);          
-            }
-            for (int i = 0; i < partsList.Count; i++)
-            {
-                partsList[i].GetComponent<Rigidbody2D>().gravityScale = 1;
-                partsList[i].GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+                break;
             }
         }
     }
